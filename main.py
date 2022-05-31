d8a21904ca83c496444cc118ff6eda9fa4830b21
vijayKumar112 <100838263+vijayKumar112@users.noreply.github.com>
2022-05-31 14:20:48 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#Write a Python script to sort (ascending and descending) a dictionary by value.
-import operator
-d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
-print('Original dictionary : ',d)
-sorted_d = sorted(d.items(), key=operator.itemgetter(1))
-print('Dictionary in ascending order by value : ',sorted_d)
-sorted_d = dict( sorted(d.items(), key=operator.itemgetter(1),reverse=True))
-print('Dictionary in descending order by value : ',sorted_d)
+#3. Write a Python script to concatenate following dictionaries to create a new one.dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic4 = {}
+for d in (dic1, dic2, dic3): dic4.update(d)
+print(dic4) 
